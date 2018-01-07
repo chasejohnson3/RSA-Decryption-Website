4ecabb5d0192c510890269f493488c32dba31b82
Chase Johnson <chaser.johnson27@gmail.com>
2018-01-06 21:31:22 -0600

    Added the line test

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
-<!DOCTYPE html>
+<!DOCTYPE HTML>
 <html>
-  <head>
-    <meta charset="UTF-8">
-    <title>title</title>
-  </head>
-  <body>
-  Hello World
-  </body>
-</html>
+
+<head>
+    <title>Home</title> <!--INSERT Title for the top tab -->
+    <!--Don't touch this code, this adds styling to the page via bootstrap -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <script src="https://code.jquery.com/jquery-3.1.0.min.js" integrity="sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s="
+        crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" type='text/css'>
+</head>
+
+test
