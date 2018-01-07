107fb6a5e190b0840d26871f71064c621ed4d02d
Chase Johnson <chaser.johnson27@gmail.com>
2018-01-06 22:26:49 -0600

    Changed the title of the website.

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>RSA Decryption</title>
+  </head>
+  <body>
+  RSA Decryptor
+  <form>
+    p
+    <input type="text" name="p">
+    q
+    <input type="text" name="q">
+    k
+    <input type="text" name="k">
+    Message:
+    <input type="text" name="msg">
+    <input type="submit" value="Decode">
+  </form>
+  </body>
+</html>
